0394b2aa28461180f4a4dcf6a5c1dc8a7e10c94b
dhruv2k02 <dhruv11@outlook.com>
2026-05-30 16:25:55 +0530

    fix: add --skip-generate to prisma db push to avoid permission error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Pushing database schema..."
-until npx prisma db push --accept-data-loss; do
+until npx prisma db push --accept-data-loss --skip-generate; do
   echo "Database is unavailable - sleeping for 5 seconds..."
   sleep 5
 done
