7cebba42b133395e3b98d214d1cadd031454cbb3
dhruv2k02 <dhruv11@outlook.com>
2026-06-01 20:45:18 +0530

    fix: hide hint until amount typed, swap txn history colors

--- app/page.js
+++ app/page.js
@@ -273,9 +273,11 @@ function LeafNode({ rootId, leaf, debts, onDebtUpdated }) {
         />
         <button className="btn icon-btn" onClick={handleTransaction}>Go</button>
       </div>
-      <div className={`sign-hint ${sign === '+' ? 'hint-out' : 'hint-in'}`}>
-        {sign === '+' ? 'You have to give' : 'You will get'}
-      </div>
+      {amount && parseInt(amount) > 0 && (
+        <div className={`sign-hint ${sign === '+' ? 'hint-out' : 'hint-in'}`}>
+          {sign === '+' ? 'You have to give' : 'You will get'}
+        </div>
+      )}
     </div>
   );
 }
